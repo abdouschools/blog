c45b8df8fb95622ba4c06349bcc67d3aa3cc30c1
abdouschools <abdouschools@gmail.com>
2021-08-12 19:00:19 +0200

    adding layouts folder and bootsrap cdn

--- templates/post/index.php
+++ templates/post/index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>mon blog</h1>
-</body>
-
-</html>
+<?php
+require TEMPLATES . '/layouts/header.php';
+?>
+<h1>blog</h1>
+<?php
+require TEMPLATES . '/layouts/footer.php';
+?>
